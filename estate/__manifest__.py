ea489eaf0a691d57c4b657cc2587749fa1cfc3d5
Pratik Raval (PRRA) <prra@odoo.com>
2023-09-04 20:15:52 +0530

    [ADD] estate: added action and menuitems

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,8 @@
     'depends':['base', 'mail'],
     'data':[
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'demo': [],
     'application':True,
